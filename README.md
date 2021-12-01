ab2432a9bf11efab9e4bf36374202033b0c23d37
onuorahmary@gmail.com <onuorahmary@gmail.com>
2021-12-02 00:19:21 +0100

    corrections

--- README.md
+++ README.md
@@ -5,7 +5,8 @@ The scope of this project covers just the landing page, Sign-in, Fix-Your-Car an
 # Running the End-To-End-tests 
 Install the dependencies. Run
 `npm install`
-Execute using `npx cypress open` or `npx cypress open`
+Execute test using `npx cypress open` or `npx cypress open`
+You may need comment out some test to run some in each of the files or attach `.only` to the test you want to run at a particular time
 
 ## Note: 
 The version of cypress used for this project is 3.0, this is due to some irregularity encountered with the current version.
--- cypress/integration/test/landingPage.test.js
+++ cypress/integration/test/landingPage.test.js
@@ -19,6 +19,41 @@ describe('Landing Page Elements',()=>{
     });
 
     it('should check that all necessary elements exists on the landing page', ()=>{
+        cy.get('.nav-items > :nth-child(2) > a > span').should('have.text', 'Fix Your Car');
+        cy.get(':nth-child(3) > a > span').should('have.text', 'Buy A Car');
+        cy.get(':nth-child(4) > a > span').should('have.text', 'Sell A Car');
+        cy.get(':nth-child(5) > a > span').should('have.text', 'Buy A Truck');
+        cy.get(':nth-child(6) > a > span').should('have.text', 'Car Loan');
+        cy.get(':nth-child(7) > a > span').should('have.text', 'Brand New');
+        cy.get('.mr-20').should('have.text', 'Sign In');
+        cy.get('[href="/en/ng/auth/register"]').should('have.text', 'Register');
+        cy.get('.buy-car').should('exist');
+        cy.get('.sell-car').should('exist');
+        cy.get('.fix-car').should('exist');
+        cy.get('.new-container > .title').should('exist');
+        cy.get(':nth-child(1) > .container > .content > .title').should('exist');
+        cy.get('.map > svg').should('exist');        
+        cy.get('a.jsx-1973554978 > .jsx-1973554978').should('exist');
+    });
+
+    it('should verify the links and buttons on the landing page work as should', ()=>{
+        cy.get('.sell-car > .section-content > .button-group > .primary-button').click();
+        cy.go('back');
+
+        cy.get('.button-group > [href="/en/ng/cars-for-sale"]').click();
+        cy.go('back');
+
+        cy.get('.button-group > [href="/en/ng/get-prequalified"]').click();
+        cy.go('back');
+
+        cy.get('.fix-car > .section-content > .button-group > .primary-button').click();
+        cy.go('back');
+
+        cy.get('.action > .primary-button').click();
+        cy.go('back');
         
-    })
-})
+        cy.get(':nth-child(1) > .reactSelectPrefix__control > .reactSelectPrefix__value-container').type('Toyota');
+        cy.get(':nth-child(2) > .reactSelectPrefix__control > .reactSelectPrefix__value-container').type('Camry');
+        cy.get('.search-cars-form > .button').click();
+    });
+});
